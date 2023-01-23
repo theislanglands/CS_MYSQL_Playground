1b144cfcf4fc9722ceec71e6566dca14ee062feb
Theis Langlands <thlan20@student.sdu.dk>
2023-01-23 15:35:53 +0100

    initial commit

---
+++ ConnectToDatabase_playground/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+using Datalayer;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataLayer dataLayer = new DataLayer();
+
+        Console.WriteLine("Testing my database");
+
+
+        dataLayer.TestingObject();
+
+        dataLayer.OpenConnection();
+
+        
+        dataLayer.ReadRecords();
+
+        dataLayer.CloseConnection();
+        
+        Console.WriteLine("All worked");
+        
+    }
+}
